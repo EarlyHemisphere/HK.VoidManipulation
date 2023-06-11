9b2970e685976f24619641f59d5ee71a9c05db4b
Lucas Anderson <lucasanderson1999@gmail.com>
2023-06-10 21:28:09 -0400

    allow multiple uses of hazard respawns

--- VoidManipulation.cs
+++ VoidManipulation.cs
@@ -1,10 +1,11 @@
 ﻿using Modding;
 using System.Collections.Generic;
+using System.Reflection;
 using UnityEngine;
 using SFCore.Utils;
 
 namespace VoidManipulation {
-    public class VoidManipulation : Mod {
+    public class VoidManipulation : Mod, ITogglableMod {
         internal static VoidManipulation instance;
 
         public VoidManipulation() : base("Void Manipulation") {
@@ -34,6 +35,22 @@ namespace VoidManipulation {
 
         public void EnableDescent(PlayMakerFSM fsm) {
             fsm.AddFsmTransition("Set", "FINISHED", "Idle");
+            HazardRespawnTrigger trigger = fsm.gameObject.GetComponent<HazardRespawnTrigger>();
+            trigger.fireOnce = false;
+            trigger.GetType().GetField("inactive", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(trigger, false);
+        }
+
+        public void Unload() {
+            GameObject ascendRespawns = GameObject.Find("Ascend Respawns");
+            if (ascendRespawns) {
+                foreach (Transform child in ascendRespawns.transform) {
+                    if (child.gameObject.name.Contains("Hazard Respawn Trigger")) {
+                        child.gameObject.LocateMyFSM("raise_abyss_pit").RemoveFsmTransition("Set", "FINISHED");
+                        HazardRespawnTrigger trigger = child.gameObject.GetComponent<HazardRespawnTrigger>();
+                        trigger.fireOnce = true;
+                    }
+                }
+            }
         }
     }
 }
